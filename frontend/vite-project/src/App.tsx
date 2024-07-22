3f0268d6748777a6c967b0cd075f10bbc4f17a14
Laxman <88457639+Laxman67@users.noreply.github.com>
2024-07-22 13:45:20 +0000

    playground

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -8,26 +8,17 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
+   <div className='container'>
+
+    <div className="image">
+    <p>Hey <br/> Dr. Aditi <br/>  I Love you 💕😍 </p>
+      <img src="
+      https://scontent.fixc2-2.fna.fbcdn.net/v/t39.30808-1/301776618_493356106131206_5910119610534176067_n.jpg?stp=dst-jpg_p200x200&_nc_cat=104&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=cB0RWPNAF5kQ7kNvgGvZkYq&_nc_ht=scontent.fixc2-2.fna&oh=00_AYDRUm74WliggQpJX6XgyX3ikmqzDFiJuTjUDJFxFHMNQw&oe=66A44423" alt="" />
+    </div>
+    <p style={{color:"white",fontWeight:"500",paddingTop:"15px"}}>Made by Laksh WITH LOVE</p>
+   </div>
+
     </>
   )
 }
